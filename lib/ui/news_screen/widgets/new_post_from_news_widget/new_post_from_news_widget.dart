39cb377348c5f8e2f504f2471b77da36bfea08fa
Богдан Лупанов <60575285+VARWA@users.noreply.github.com>
2023-08-07 20:04:25 +0300

    updated new post design

--- lib/ui/news_screen/widgets/new_post_from_news_widget/new_post_from_news_widget.dart
+++ lib/ui/news_screen/widgets/new_post_from_news_widget/new_post_from_news_widget.dart
@@ -11,61 +11,85 @@ class NewPostFromNewsWidget extends StatelessWidget {
       elevation: 6,
       color: Colors.white,
       surfaceTintColor: Colors.white,
-      child: Column(
-        children: [
-          const Align(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Align(
               alignment: AlignmentDirectional.topStart,
               child: Padding(
-                padding: EdgeInsets.only(
+                padding: const EdgeInsets.only(
                   top: 8,
                   left: 8,
                 ),
                 child: Row(
                   children: [
-                    SizedBox(
-                      height: 20,
+                    const SizedBox(
+                      height: 30,
                       child: CircleAvatar(
                         backgroundImage: NetworkImage(
                             'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAsdThKP8w9qAG1vLbSlWcZFT17_XXHnPJ_kBLfaFo&s'),
                       ),
                     ),
-                    Text('RSHB Identity Management Meetup'),
-                  ],
-                ),
-              )),
-          Align(
-            alignment: AlignmentDirectional.bottomEnd,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text('Хочешь чем-то поделиться?'),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: const ImageIcon(
-                        AssetImage('assets/icons/document/document.png'),
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const ImageIcon(
-                        AssetImage('assets/icons/location/location.png'),
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const ImageIcon(
-                        AssetImage('assets/icons/gallery/gallery.png'),
+                    Text(
+                      'RSHB Identity Management Meetup',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
-          )
-        ],
+            Align(
+              alignment: AlignmentDirectional.bottomEnd,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  const Text('Хочешь чем-то поделиться?'),
+                  Row(
+                    children: [
+                      SizedBox(
+                        height: 25,
+                        child: GestureDetector(
+                          child: const ImageIcon(
+                            AssetImage('assets/icons/document/document.png'),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 5),
+                      SizedBox(
+                        height: 25,
+                        child: GestureDetector(
+                          child: const ImageIcon(
+                            AssetImage('assets/icons/location/location.png'),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 5),
+                      SizedBox(
+                        height: 40,
+                        child: SizedBox(
+                          height: 25,
+                          child: GestureDetector(
+                            child: const ImageIcon(
+                              AssetImage('assets/icons/gallery/gallery.png'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 2),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
