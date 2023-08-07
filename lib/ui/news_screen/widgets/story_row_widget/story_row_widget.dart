6e4f57c36d3e78deb905a8f989a6ebb9d31b6547
Богдан Лупанов <60575285+VARWA@users.noreply.github.com>
2023-08-07 20:04:51 +0300

    updated stories, added paddings

--- lib/ui/news_screen/widgets/story_row_widget/story_row_widget.dart
+++ lib/ui/news_screen/widgets/story_row_widget/story_row_widget.dart
@@ -27,16 +27,46 @@ class StoryRowWidget extends StatelessWidget {
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjpe4Q3pd8Ci6DxGT70vlbUUTYBq-35ZwnH6clmlfK&s',
     ];
 
-    return Container(
-      height: 80,
+    return SizedBox(
+      height: 60,
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Row(
           children: imagePathes
               .map(
-                (e) => StoryItemWidget(imagePath: e),
+                (e) => Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 4.0,
+                  ),
+                  child: StoryItemWidget(imagePath: e),
+                ),
               )
-              .toList(),
+              .toList()
+            ..insert(
+              0,
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 4,
+                ),
+                child: Badge(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  alignment: AlignmentDirectional.bottomEnd,
+                  offset: Offset(-3, -3),
+                  label: const Icon(
+                    Icons.add,
+                    size: 10,
+                    color: Colors.white,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAsdThKP8w9qAG1vLbSlWcZFT17_XXHnPJ_kBLfaFo&s'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
         ),
       ),
     );
